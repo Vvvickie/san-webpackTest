fcd4a76a73d707b6c4091a001f3052e7a52025e2
haoyan <haoyan@iscas.ac.cn>
2018-07-25 17:53:00 +0800

    hello san

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+- npm install
+
+- npx webpack
+
+open dist/index.html and you will see the element created in src/index.js
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import pic from './1.png';
+import prinntMe from './print';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'san'], ' ');
+  element.innerHTML = _.join(['Hello', 'ww'], ' ');
+  element.classList.add('hello');
+
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = prinntMe;
+
+  var myPic = new Image();
+  myPic.src = pic;
+
+  element.appendChild(myPic);
+  element.appendChild(btn);
 
   return element;
 }
