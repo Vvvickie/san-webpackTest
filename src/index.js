b8f4a88126d48716af85c39b02209a58877df226
haoyan <haoyan@iscas.ac.cn>
2018-07-26 18:37:23 +0800

    hello san

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
   var btn = document.createElement('button');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'ww'], ' ');
+  element.innerHTML = _.join(['Hello', 'ww!'], ' ');
   element.classList.add('hello');
 
   btn.innerHTML = 'click me and check the console';
@@ -24,3 +24,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   // entry: './src/index.js',
   entry: {
     app: './src/index.js',
-    print:'./src/print.js'
+    // print:'./src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title:'Output Management'
-    })
+      title: 'Output Management',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
